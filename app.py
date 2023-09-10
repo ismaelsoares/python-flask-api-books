8327e7a8a4bcc61b14514abbc0bf296c8b655bc9
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2023-09-10 20:50:17 -0300

    Add pip requirements.txt

--- app.py
+++ app.py
@@ -10,33 +10,16 @@
     4. Quais recursos - Livros
 """
 from flask import Flask, jsonify, request
+from books import books
 
 app = Flask(__name__)
 
-books = [
-    {
-        'id': 1,
-        'title': 'O Senhor dos Anéis - A Sociedade do Anel',
-        'author': 'J.R.R Tokien',
-    },
-    {
-        'id': 2,
-        'title': 'O Senhor dos Anéis - As Duas Torres',
-        'author': 'J.R.R Tokien',
-    },
-    {
-        'id': 3,
-        'title': 'O Senhor dos Anéis - O Retorno do Rei',
-        'author': 'J.R.R Tokien',
-    }
-]
 
 # Consultar(todos)
-
-
 @app.route('/books', methods=['GET'])
 def get_books():
     return jsonify(books)
+
 
 # Criar livro
 
---
+++ books.py
@@ -0,0 +1,17 @@
+books = [
+    {
+        'id': 1,
+        'title': 'O Senhor dos Anéis - A Sociedade do Anel',
+        'author': 'J.R.R Tokien',
+    },
+    {
+        'id': 2,
+        'title': 'O Senhor dos Anéis - As Duas Torres',
+        'author': 'J.R.R Tokien',
+    },
+    {
+        'id': 3,
+        'title': 'O Senhor dos Anéis - O Retorno do Rei',
+        'author': 'J.R.R Tokien',
+    }
+]
